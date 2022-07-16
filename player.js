0d952e993b2c58204ce35ce52a60d39de64daf62
Ali Arif <91418777+aliarif1@users.noreply.github.com>
2022-07-16 05:13:37 -0700

    Update player.js

--- player.js
+++ player.js
@@ -2,9 +2,10 @@
 var files = [
   "files/sounds/bleep.mp3",
   "files/sounds/ding.mp3",
-  "files/sounds/suspend.mp3"
+  "files/sounds/twig.mp3"
 ];
 
+// For the pause functionality
 var resume = false;
 
 // Current index of the files array
@@ -15,24 +16,26 @@ var music_player = document.querySelector("#music_list audio");
 
 // function for moving to next audio file
 function next() {
+  // Resume the previously paused audia
   if (resume) {
     music_player.play();
     resume = false;
   } else {
+    // Change the audio element source to a new random audio file
     index = Math.floor(Math.random() * 3);
-    // Change the audio element source
     music_player.src = files[index];
     resume = false;
   }
 }
 
+// Start the player
 function play() {
-  // Start the player
   next();
   // Listen for the music ended event, to play the next audio file
   music_player.addEventListener('ended', next, false)
 }
 
+// To pause the audio playback
 function pause() {
   resume = true;
   document.getElementById("audio").pause();
